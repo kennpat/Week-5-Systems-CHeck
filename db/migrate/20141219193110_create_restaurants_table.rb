31f51120d9f339004bb319a648d1058b3e753eb0
kennpat <kennedypr@gmail.com>
2014-12-19 14:48:11 -0500

    this is the initial commit, database setup completed, controller files added, model files added

---
+++ db/migrate/20141219193110_create_restaurants_table.rb
@@ -0,0 +1,14 @@
+class CreateRestaurantsTable < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+    	t.string :name, null: false
+    	t.string :address, null: false
+    	t.string :city, null: false
+    	t.string :state, null: false
+    	t.integer :zip_code, null: false
+    	t.string :description
+
+    	t.timestamps
+    end
+  end
+end
